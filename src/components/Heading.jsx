c81a96f657232f4f10d65d5b2db37bb3ef9d2d66
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-07-10 17:52:35 +0300

    adding tailwind to the app

---
+++ src/components/Heading.jsx
@@ -0,0 +1,13 @@
+function Heading() {
+  return (
+    <div>
+      <div>
+        <img src="/src/assets/images/icon-star.svg" alt="" />
+      </div>
+      
+      <h1>FAQs</h1>
+    </div>
+  );
+}
+
+export default Heading;
---
+++ src/components/Questions.jsx
@@ -0,0 +1,9 @@
+function Questions() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Questions
