7674b4de03d1728e9b26e96a0c096c7d739e89fe
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-07-11 14:15:25 +0300

    upload latest css updates

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 function Questions({ questions }) {
+
   return (
     <ul>
       {questions.map((question) => (
         <details
           key={question.id}
-          className="cursor-pointer p-5 [&:not(:last-child)]:border-b border-lightPink w-[445px]"
+          className="cursor-pointer p-5 [&:not(:last-child)]:border-b border-lightPink w-[445px] ease-in-out duration-300"
         >
           <summary className="flex justify-between">
-            <h3 className="text-darkPurple font-bold">{question.question}</h3>
+            <h3 className="text-darkPurple font-bold hover:text-[#a23ae6]">
+              {question.question}
+            </h3>
 
-            <span>
-              <img src="/src/assets/images/icon-plus.svg" alt="" />
+            <span className="w-8">
+              <img src="/src/assets/images/icon-plus.svg" alt="plus" />
             </span>
           </summary>
 
